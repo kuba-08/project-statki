5412d7a6d8d95fe02ae1bfe6b16e3e4e039d46a0
kuba-08 <kubakieca2008@gmail.com>
2024-12-05 12:22:13 +0100

    Ship.h

--- Ship.h
+++ Ship.h
@@ -6,18 +6,21 @@ class Ship
 private:
     int size;            // Rozmiar statku (liczba masztów)
     int hits;            // Liczba trafień
-    int row;             // Wiersz początkowy statku
-    int col;             // Kolumna początkowa statku
-    bool horizontal;     // Orientacja statku: true = poziomy, false = pionowy
-public:
-    Ship(int size, int row, int col, bool horizontal);
     
+    bool horizontal;     // Orientacja statku: true = poziomy, false = pionowy
+    int startX;
+    int startY;
+    bool isSunkFlag;
+public:
+    Ship(int size, int startX, int startY, bool horizontal);
+    bool occupiesPosition(int x, int y) const;
     bool isSunk() const;                 // Sprawdza, czy statek jest zatopiony
     void hit();                          // Zwiększa liczbę trafień
     int getSize() const;                 // Zwraca rozmiar statku
     bool getOrientation() const;         // Zwraca orientację statku
-    int getRow() const;                  // Zwraca początkowy wiersz statku
-    int getCol() const;                  // Zwraca początkową kolumnę statku
+    int getstartX() const;                  // Zwraca początkowy wiersz statku
+    int getstartY() const;                  // Zwraca początkową kolumnę statku
+
     
 
 };
